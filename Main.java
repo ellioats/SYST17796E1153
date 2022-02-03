e9d943204829c3a11d84ec5342f067c2ee72d22c
ellioats <98979490+ellioats@users.noreply.github.com>
2022-02-03 13:48:46 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+// Elliot Currie
+// Date: 2/3/22
+// Purpose: A class that generates a random array of numbers which represent various temperatures, these are then averaged in the calcAvg() method
+
+
+public class Main {
+
+    public static void main (String[] args){
+
+        int _max = 50;
+        int _min = -50;
+
+
+        int[] temps = new int[14];
+        for (int i = 0; i < 14; i++) {
+            temps[i] = (int) (Math.random() * (_max - _min)+ _min);
+            System.out.println(temps[i]);
+        }
+
+        System.out.println(calcAvg(temps));
+
+    }
+
+    private static int calcAvg(int[] temps) {
+        int avgSum = 0;
+
+        for (int i = 0; i < temps.length; i++) {
+            avgSum += temps[i];
+        }
+
+        return (avgSum / temps.length);
+
+    }
+
+}
